8f000ad71c100da0118de554f8631d6ce7d83ce8
Anwar Ziani <anwaar.ziani@gmail.com>
2019-05-19 17:16:38 -0700

    Create cars migration

---
+++ database/migrations/2019_05_19_234429_create_cars_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 1000);
+            $table->string('registration_number');
+            $table->integer('production_year');
+            $table->double('price');
+            $table->double('kilometers');
+            $table->integer('seats');
+            $table->string('fuel');
+            $table->string('speed');
+
+            $table->unsignedBigInteger('agency_id');
+            $table->unsignedBigInteger('model_id');
+
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
