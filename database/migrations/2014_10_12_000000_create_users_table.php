fdfd4b40d8f3b37b2fb008087f50d1cc2e8d0aaa
Ayoub Ziani <Aayoub056@gmail.com>
2019-05-20 03:45:35 +0000

    update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('first_name',['admin','moderateur']);
+
+            $table->integer('agency_id');
+
             $table->rememberToken();
             $table->timestamps();
         });
