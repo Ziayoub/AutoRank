adc1f5e2579c9db25d08f6318ef97b2282f8d3e4
Ayoub Ziani <Aayoub056@gmail.com>
2019-05-21 05:25:28 +0000

    add 'approved' column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role',['admin','moderateur']);
+            $table->boolean('approved');
 
             $table->integer('agency_id');
 
