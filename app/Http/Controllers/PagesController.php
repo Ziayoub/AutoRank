5750e04ed3c6fa1bfe3206cb49b1fa403355a0e7
Ayoub Ziani <Aayoub056@gmail.com>
2019-05-26 04:16:01 +0000

    add pages controllers

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public  function about()
+    {
+        return view('about');
+    }
+
+    public  function contact()
+    {
+        return view('contact');
+    }
+
+    public  function agencies()
+    {
+        return view('agencies');
+    }
+
+    public  function agency()
+    {
+        return view('agency');
+    }
+
+    public  function cars()
+    {
+        return view('cars');
+    }
+
+    public  function car()
+    {
+        return view('car');
+    }
+
+
+
+
+}
